ca5867f78f54dc0da55fe89bbffc73a44c8cbf5e
Rui Fangueiro <rui_fangueiro@yahoo.com>
2024-03-22 12:08:45 +0000

    initial

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,26 @@
-import socket
+import socket 
+from typing import Union, List
 
-def get_open_ports(target, port_range, verbose = False):
-    open_ports = []
-
-
+def get_open_ports(target: Union[int, str], port_range: List[int], verbose = False):
+    open_ports = []    
+    if isinstance(target, int):
+        target = socket.inet_ntoa(target.to_bytes(4, 'big'))
+    elif isinstance(target, str):
+        try:
+            ip_adress = socket.gethostbyname(target)
+            target = ip_adress
+        except socket.error:
+            try:
+                socket.inet_aton(target)
+            except socket.error:
+                return f"Error: Invalid hostname"
+            except socket.gaierror:
+                return f"Error: Invalid hostname"
+                
+    for port in range(port_range[0], port_range[1]+1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)
+        if s.connect_ex((target, port)) == 0:
+            open_ports.append(port)
 
     return(open_ports)
